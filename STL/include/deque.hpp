088664fa586ca28ceca02a93217ee66b35355cf6
liu <liuhao20240625@163.com>
2025-12-25 09:05:07 +0800

    deque函数实现

--- STL/include/deque.hpp
+++ STL/include/deque.hpp
@@ -44,7 +44,7 @@ public:
     void assign(std::initializer_list<T> init);
 
     //尾插和尾删//头插和头删
-    void push_back( const T&val);
+    void push_back(const T&val);
     void push_bach(T&& val);
     void pop_back();
     void push_front( const T&val);
@@ -93,6 +93,7 @@ private:
     //重新分配内存辅助函数：
     void allocate_map(size_t new_map_size);
     void allocate_blocks(size_t start, size_t end);
+    void destroy_elements();
     void deallocate_blocks(size_t start, size_t end);
     
     //内部迭代器位置计算
@@ -291,13 +292,540 @@ public:
     const_iterator() : m_current_block(nullptr), m_current(nullptr),
                       m_block_begin(nullptr), m_block_end(nullptr){}
     const_iterator(T** block_ptr, const T* elem_ptr) :
-        m_current_block(block_ptr), m_current(elem_ptr)
-    {
-        
+        m_current_block(block_ptr), m_current(elem_ptr){
+        if(block_ptr && *block_ptr){
+            m_block_begin = *block_ptr;
+            m_block_end = *block_ptr + BLOCK_SIZE;
+        }else{
+            m_block_begin = m_block_end = nullptr;
+        }
+    }
+
+    const_iterator(const iterator& other):
+    m_current_block(other.m_current_block),
+    m_current(other.m_current),
+    m_block_begin(other.m_block_begin),
+    m_block_end(other.m_block_end){}
+
+    //迭代器操作
+    reference operator*() const { return *m_current; }
+    pointer operator->() const { return m_current; }
+
+    const_iterator& operator++() {
+        ++m_current;
+        if (m_current == m_block_end) {
+            ++m_current_block;
+            m_block_begin = *m_current_block;
+            m_block_end = m_block_begin + BLOCK_SIZE;
+            m_current = m_block_begin;
+        }
+        return *this;
+    }
+
+    const_iterator operator++(int) {
+        const_iterator tmp = *this;
+        ++(*this);
+        return tmp;
+    }
+
+    const_iterator& operator--() {
+        if(m_current == m_block_begin) {
+            --m_current_block;
+            m_block_begin = *m_current_block;
+            m_block_end = m_block_begin + BLOCK_SIZE;
+            m_current = m_block_end;
+        }
+        --m_current;
+        return *this;
+    }
+
+    const_iterator operator--(int) {
+        const_iterator tmp = *this;
+        --(*this);
+        return tmp;
+    }
+
+    const_iterator& operator+=(difference_type n){
+        if( n>=0 ) {
+            while(n > 0) {
+                difference_type elements_in_block =m_block_end - m_current;
+                if( n < elements_in_block){
+                    m_current +=n;
+                    break;
+                }
+                n -= elements_in_block;
+                ++m_current_block;
+                m_block_begin = *m_current_block;
+                m_block_end = m_block_begin + BLOCK_SIZE;
+                m_current = m_block_begin;
+            }
+        } else{
+            operator-=(-n);
+        }
+        return *this;
+    }
+
+    const_iterator operator+(difference_type n) const {
+        const_iterator tmp = *this;
+        tmp +=n;
+        return tmp;
+    }
+    const_iterator operator-(difference_type n) const {
+        const_iterator tmp = *this;
+        tmp -=n;
+        return tmp;
+    }
+
+    friend const_iterator operator+(difference_type n, const const_iterator& it){
+        return it + n;
+    }
+
+    difference_type operator-(const const_iterator& other) const{
+        if(m_current_block == other.m_current_block){
+            return m_current - other.m_current;
+        }
+        difference_type blocks_diff = m_current_block - other.m_current_block;
+        return blocks_diff * BLOCK_SIZE + 
+               (m_current - m_block_begin) -
+               (other.m_current - other.m_block_begin);
+    }
+
+    bool operator ==(const const_iterator& other) const{
+        return m_current == other.m_current;
+    }
+    bool operator !=(const const_iterator& other) const{
+        return !(*this == other);
+    }
+    bool operator <(const const_iterator& other) const{
+        if(m_current_block == other.m_current_block){
+            return m_current < other.m_current;
+        }
+        renturn m_current_block < other.m_current_block;
+    }
+    bool operator >(const const_iterator& other) const{
+        return other < *this;
+    }
+    bool operator <=(const const_iterator& other) const{
+        return !(*this > other);
+    }
+    bool operator >=(const const_iterator& other) const{
+        return !(*this < other); 
+    }
+
+
+    // 辅助函数
+    void set_block(T** new_block){
+        m_current_block = new_block;
+       m_block_begin = *new_block;
+       m_block_end = m_block_begin + BLOCK_SIZE;
+    }
+    
+};
+
+
+
+
+template <class T>
+deque<T>::deque(const size_t n, const T& val)
+:m_map(nullptr),
+ m_map_size(0),
+ m_start_block(0),
+ m_start_index(0),
+ m_size(0){
+    
+    if(n == 0){
+        allocate_map(MAP_INIT_SIZE);
+        return;
+    }
+    size_t needed_blocks = (n + BLOCK_SIZE - 1) / BLOCK_SIZE;
+
+    m_map_size = std::max(MAP_INIT_SIZE, needed_blocks + 2);
+
+    allocate_map(m_map_size);
+    
+    m_start_block = (m_map_size - needed_blocks) / 2;
+    m_start_index = 0;
+    m_size = n;
+    try{
+
+        allocate_blocks(m_start_block, m_start_block + needed_blocks);
+
+        size_t current_block = m_start_block;
+        size_t remaining = n;
+        T* block_ptr = m_map[current_block];
+        size_t index_in_block = 0;
+
+        while (remaining > 0) {
+
+            size_t elements_in_this_block = std::min(remaining, BLOCK_SIZE -index_in_block);
+
+            for (size_t i = 0; i < elements_in_this_block; ++i){
+                new (block_ptr + index_in_block + i) T(val);
+            }
+            remaining -= elements_in_this_block;
+            index_in_block += elements_in_this_block;
+
+            if( index_in_block == BLOCK_SIZE && remaining > 0) {
+                ++current_block;
+                block_ptr = m_map[current_block];
+                index_in_block = 0;
+            }
+        }    
+    }
+    catch(...){
+        destroy_elements();
+        deallocate_blocks(m_start_block, m_start_block + (n + BLOCK_SIZE - 1 ) / BLOCK_SIZE);
+
+        if (m_map) {
+            delete[] reinterpret_cast<char*>(m_map);
+            m_map = nullptr;
+        }
+        throw;       
     }
 };
 
 template <class T>
-deque<T>::deque(const size_t n, const T& val){
+deque<T>::deque (const deque<T>& other){
+    m_map = nullptr;
+    m_map_size = 0;
+    m_size = 0;
+    m_start_block = 0;
+    m_start_index = 0;
 
-};
+    if(other.empty()){
+        allocate_map(MAP_INIT_SIZE);
+        return;
+    }
+    size_t needed_blocks = (other.m_size + BLOCK_SIZE - 1)/ BLOCK_SIZE;
+
+    m_map_size = other.m_map_size;
+    allocate_map(m_map_size);
+
+    m_start_block = other.m_start_block;
+    m_start_index = other.m_start_index;
+    m_size = other.m_size;
+
+    try{
+
+        size_t first_used_block = m_start_block;
+        size_t last_used_block = m_start_block + needed_blocks - 1;
+        if (m_start_index > 0 && needed_blocks >1){
+            last_used_block++;
+        }
+        for (size_t = first_used_block; i<= last_used_block; ++i){
+            if(other.m_map[i]){
+                m_map[i] = reinterpret_cast<T*>(new char[BLOCK_SIZE * sizeof(T)]);
+            }
+        }
+        auto src_it = other.begin();
+        auto dst_it = begin();
+
+        while (src_it != other.end()){
+
+            new (&(*dst_it)) T(*src_it);
+            ++src_it;
+            ++dst_it;
+        }
+
+    }
+    catch(...){
+        clear();
+        if(m_map){
+            delete[] reinterpret_cast<char*>(m_map);
+            m_map = nullptr;
+        }
+        throw;
+    }
+}
+
+  //移动构造
+template<class T>
+deque<T>::deque(deque<T>&& other)noexcept
+    :m_map(other.m_map),
+     m_map_size(other.m_map_size),
+     m_start_block(other.m_start_block),
+     m_start_index(other.m_start_index),
+     m_size(other.m_size){
+    
+    //将other置为空状态
+    other.m_map = nullptr;
+    other.m_map_size = 0;
+    other.m_start_block = 0;
+    other.m_start_index = 0;
+    other.m_size = 0;
+}
+
+template<class T>
+deque<T>::deque(std::initializer_list<T> init)
+: m_map(nullptr),
+  m_map_size(0),
+  m_start_block(0),
+  m_start_index(0),
+  m_size(0){
+    if(init.size()>0){
+        size_t needed_blocks = (init.size() + BLOCK_SIZE - 1) / BLOCK_SIZE;
+        
+        m_map_size = std::max(MAP_INIT_SIZE, needed_blocks + 4);
+        allocate_map(m_map_size);
+
+        m_start_block = m_map_size / 2 - needed_blocks / 2;
+        m_start_index = 0;
+        m_size = init.size();
+
+        for (size_t i = 0; i < needed_blocks; ++i){
+            m_map[m_start_block + i] = reinterpret_cast<T*>(new char[BLOCK_SIZE * sizeof(T)]);
+        }
+
+        auto src =init.begin();
+        for(size_t block =0; block < needed_blocks; ++block){
+            T* block_ptr = m_map[m_start_block + block];
+            size_t elements_in_block = (block == needed_blocks -1)
+            ?(init.size() - block*BLOCK_SIZE)
+            :BLOCK_SIZE;
+
+            for (size_t i = 0; i < elements_in_block; ++i){
+                new(block_ptr + i) T(*src++);
+            }
+        }
+    }else{
+        allocate_map(MAP_INIT_SIZE);
+    }
+}
+
+//析构函数
+template<class T>
+deque<T>::~deque (){}
+
+//待验证
+template<class T>
+deque<T>& deque<T>::operator =(std::initializer_list<T> init){
+
+    if(init.size() == 0) {
+        clear();
+        return *this;
+    }
+
+    try {
+
+        deque<T temp;
+
+        size_t needed_blocks = (init.size() + BLOCK_SIZE - 1) / BLOCK_SIZE;
+        size_t new_map_size = std::max(MAP_INIT_SIZE, needed_blocks * 2);
+
+        temp.allocate_map(new_map_size);
+        temp.m_start_block = (new_map_size - needed_blocks) / 2;
+        temp.m_start_index = 0;
+        temp.m_size = init.size();
+        temp.m_map_size = new_map_size;
+
+        temp.allocate_blocks(temp.m_start_block, temp.m_start_block + needed_blocks);
+
+        auto src = init.begin();
+        for (size_t block_idx = 0; block_idx < needed_blocks; ++block_idx){
+            T* block_ptr = temp.m_map[temp.m_start_block +block_idx];
+            size_t elements_in_block = (block_idx == needed_blocks - 1)
+            ? (init.size() - block_idx * BLOCK_SIZE)
+            :BLOCK_SIZE;
+
+        for (size_t i = 0; i < elements_in_block; ++i) {
+            new (block_ptr + i) T(*src++);
+            }
+        }
+        swap(temp);
+    }catch(...){
+        throw;
+    }
+    
+    return *this;
+}
+
+
+template<class T>
+deque<T>& deque<T>::operator=(deque<T>&& other)noexcept{
+    if (this != &other) {
+        clear();
+    }
+    if(m_map) {
+        delete[] reinterpret_cast<char*>(m_map);
+    }
+    m_map = other.m_mapp;
+    m_map_size = other.m_map_size;
+    m_star_index = other.m_start_index;
+    m_size = other.m_size;
+
+    other.m_map = nullptr;
+    other.m_map_size = 0;
+    other.m_start_block = 0;
+    other.m_start_index = 0;
+    other.m_size = 0;
+}
+
+template<class T>
+void deque<T>::assign(size_t n, const T&val){
+    if( n == 0) {
+        clear();
+        return;
+    }
+
+    deque<T> temp;
+
+    try{
+        size_t needed_blocks = (n+ BLOCK_SIZE - 1) / BLOCK_SIZE;
+        size_t map_size = std::max(MAP_INIT_SIZE, needed_blocks * 2);
+
+        temp.allocate_map(map_size);
+        temp.m_start_block = (map_size - needed_blocks) / 2;
+        temp.m_start_index = 0;
+        temp.m_size = n;
+        temp.m_map_size = map_size;
+
+        temp.allocate_blocks(temp.m_start_block, temp.m_start_block + needed_blocks);
+
+        size_t current_block = temp.m_start_block;
+        size_t remaining = n;
+        size_t index_in_block = 0;
+
+        while (remaining > 0)
+        {
+            T* block_ptr = temp.m_map[current_block];
+            size_t elements_in_this_block = std:: min(remaining, BLOCK_SIZE - index_in_block);
+
+            for(size_t i = 0; i < elements_in_this_block; ++i){
+                new( block_ptr + index_in_block + i) T(val);
+            }
+
+            remaining -= elements_in_this_block;
+            if(index_in_block + elements_in_this_block == BLOCK_SIZE && remaining > 0){
+                current_block++;
+                index_in_block = 0;
+            } else{
+                index_in_block +=elements_in_this_block;
+            }
+        }
+
+        swap(temp);
+        
+    }catch(...){
+        throw;
+    }
+}
+
+template<class T>
+void deque<T>::assign (T* begin_ptr, T* end_ptr){
+    if(!begin_ptr || !end_ptr || begin_ptr > end_ptr){
+        throw std::invalid_argument("Invalid pointer range");
+    }
+
+    size_t n = end_ptr - begin_ptr;
+
+    if (is_range_within_deque(begin_ptr, end_ptr)){
+
+        vector<T> temp(begin_ptr, end_ptr);
+        deque<T> new_deque(temp.data(), temp.data() + temp.size());
+        swap(new_deque); 
+    } else {
+        deque<T> new_deque(begin_ptr, end_ptr);
+        swap(new_deque);
+    }
+}
+
+template<class T>
+void deque<T>::assign(std::initializer_list<T> init){
+    *this = init;
+}
+
+template<class T>
+void deque<T>::push_back(const T&val){
+
+    if(m_size == 0){
+        if (m_map == nullptr){
+            allocate_map(MAP_INIT_SIZE);
+            m_start_block = m_map_size / 2;
+            m_start_index = 0;
+        }
+
+        if(m_map[m_start_block] == nullptr){
+            m_map[m_start_block] = reinterpret_cast<T*>(
+                new char[BLOCK_SIZE * sizeof(T)];
+            )
+        }
+        new (m_map[m_start_block] + m_start_index) T(val);
+        m_size = 1;
+        return;
+    }
+    //插入新元素；
+}
+
+template<class T>
+typename deque<T>::iterator deque<T>::begin(){
+    if(empty()){
+        return iterator(nullptr, nullptr);
+    }
+    return iterator(& m_map[m_start_block], m_map[m_start_block] + m_start_index);
+}
+
+template<class T>
+typename deque<T>::iterator deque<T>::end(){
+    if(empty()){
+        return iterator(nullptr, nullptr)
+    }
+    size_t total_elements = m_start_inex + m_size ;
+    size_t last_block = m_start_block + (total_elements - 1)/ BLOCK_SIZE;
+    size_t index_in_last_block = (total_elements - 1)% BLOCK_SIZE; 
+
+    return iterator(&m_map[last_block],m_map[last_block] + index_in_last_block +1);
+}
+
+
+//辅助函数实现
+template <class T>
+void deque<T>::allocate_map(size_t new_map_size){
+
+    m_map = reinterpret_cast<T**>(new char[new_map_size * sizeof(*T)]);
+    m_map_size = new_map_size;
+
+    for(size_t i= 0; i < m_map_size; ++i){
+        m_map[i] = nullptr;
+    }
+}
+
+template <class T>
+void deque<T>::allocate_blocks(size_t start_block, size_t end_block){
+
+    for (size_t i = start_block; i < end_block; ++i){
+        m_map[i] = reinterpret_cast<T*>(new char[BLOCK_SIZE * sizeof(T)]);
+    }
+}
+
+template <class T>
+void deque<T>::destroy_elements(){
+    if (m_size == 0 || !m_map ) return;
+
+    size_t current_block = m_start_block;
+    size_t index_in_block = m_start_index;
+    size_t remaining = m_size;
+
+    while (remaining > 0) {
+        T* block_ptr = m_map[current_block];
+        size_t elements_in_this_block = std::min(remaining, BLOCK_SIZE -index_in_block);
+
+        for (size_t i = 0; i < elements_in_this_block; ++i){
+            (block_ptr + index_in_block + i)->~T();
+        }
+        remaining -= elements_in_this_block;
+        index_in_block = 0;
+        ++current_block;
+    }
+}
+
+template <class T>
+void deque<T>::deallocate_blocks(size_t start_block, size_t end_block) {
+
+    for(size_t i = start_block; i < end_block; ++i){
+        if (m_map[i]){
+
+            delete[] reinterpret_cast<char*>(m_map[i]);
+            m_map[i] = nullptr;
+        }
+    }
+} 
